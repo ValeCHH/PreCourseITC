188ba0bb008efbb8cb45d9286e07524bf04bf040
ValeChH <93483575+ValeCHH@users.noreply.github.com>
2021-11-15 09:26:50 +0200

    Add files via upload

--- Cherniavsky_Vale_ITCPrecourse/js/main.js
+++ Cherniavsky_Vale_ITCPrecourse/js/main.js
@@ -1,13 +1,6 @@
 //replace with your user:
 const GITHUB_URL = "https://github.com/ValeCHH/vale.github.git";
 
-fetch(GITHUB_URL)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function (data) {
-    const profileImage = document.getElementById("profile-image");
-    profileImage.src = C:\Users\valec\OneDrive\Escritorio\PreCourse\Cherniavsky_Vale_ITCPrecourse\images\hello.gif;
-  });
-  const (codingLanguages) = ["HTML", "CSS", "JavaScript"]
+
+  
 
